a8115b1ff0cde427c958b9127f67a270a5cc954a
David C. Bishop <david@davidbishop.org>
2014-12-21 09:21:08 +1100

    Check if files exist.

--- dwi.go
+++ dwi.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path"
 )
 
 func main() {
-	Main(os.Args, os.Stdout, os.Stderr, NewLocalFiles("/"))
+	lf := NewLocalFiles("")
+	Main(os.Args, os.Stdout, os.Stderr, &lf)
 }
 
 // The programs usage help.
@@ -19,11 +21,25 @@ Usage:
 
 // FileIndex provides methods for manipulating a list of files.
 type FileIndex interface {
+	Exists(filename string) bool
 }
 
 // LocalFiles is a FileIndex for files in the local filesystem.
 type LocalFiles struct {
 	Root string
+}
+
+// Exists returns true if a file given by path exists.
+func (lf *LocalFiles) Exists(filename string) bool {
+	if _, err := os.Stat(lf.getFullPath(filename)); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+// GetFullPath returns the full path.
+func (lf *LocalFiles) getFullPath(filename string) string {
+	return path.Join(lf.Root, filename)
 }
 
 // NewLocalFiles creates and initializes a new LocalFiles with a given path root.
@@ -32,14 +48,29 @@ func NewLocalFiles(root string) LocalFiles {
 }
 
 // Main function.
-func Main(args []string, out io.Writer, eout io.Writer, fm FileIndex) {
+func Main(args []string, out io.Writer, eout io.Writer, fi FileIndex) {
 	if len(args) < 2 {
 		fmt.Fprintln(out, Usage)
 		return
 	}
 
 	files := ParseArgs(args)
-	fmt.Fprintln(out, files)
+	if AnyFilesDontExist(eout, fi, files) {
+		return
+	}
+}
+
+// AnyFilesDontExist will look for each of the given files in the given FileIndex,
+// output an error to eout for any file that doesn't exist and return true if where any missing.
+func AnyFilesDontExist(eout io.Writer, fi FileIndex, files []string) bool {
+	err := false
+	for _, f := range files {
+		if !fi.Exists(f) {
+			fmt.Fprintln(eout, "Error: File not found:", f)
+			err = true
+		}
+	}
+	return err
 }
 
 // ParseArgs takes a list of args and returns the list of files.
--- dwi_test.go
+++ dwi_test.go
@@ -7,9 +7,9 @@ import (
 
 func TestMainNoArgsShouldPrintUsage(t *testing.T) {
 	out := bytes.NewBufferString("")
-	err := bytes.NewBufferString("")
 
-	Main([]string{""}, out, err, NewLocalFiles("/"))
+	lf := NewLocalFiles("")
+	Main([]string{""}, out, out, &lf)
 
 	if out.String() != Usage+"\n" {
 		t.Error("Did not print usage.")
