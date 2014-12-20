e377cb3f2d1bd21bccab3d14ac2fcca32c68fe90
David C. Bishop <david@davidbishop.org>
2014-12-21 09:26:59 +1100

    Add Test for missing file.

--- dwi_test.go
+++ dwi_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"log"
 	"testing"
 )
 
@@ -12,6 +13,19 @@ func TestMainNoArgsShouldPrintUsage(t *testing.T) {
 	Main([]string{""}, out, out, &lf)
 
 	if out.String() != Usage+"\n" {
+		t.Error("Did not print usage.")
+	}
+}
+
+func TestMainMissingFile(t *testing.T) {
+	missingFilename := "thisfileshouldntexist_asd3f4f2tfdsfa"
+	out := bytes.NewBufferString("")
+
+	lf := NewLocalFiles("")
+	Main([]string{"dealwithit", missingFilename}, out, out, &lf)
+
+	if out.String() != "Error: File not found: "+missingFilename+"\n" {
+		log.Println(out.String())
 		t.Error("Did not print usage.")
 	}
 }
