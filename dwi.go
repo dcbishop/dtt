61e805ea76e3ee9835f1a97acd58c5d88bd252c9
David C. Bishop <david@davidbishop.org>
2014-12-21 09:03:23 +1100

    Add FileIndex.

--- dwi.go
+++ dwi.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	Main(os.Args, os.Stdout, os.Stderr)
+	Main(os.Args, os.Stdout, os.Stderr, NewLocalFiles("/"))
 }
 
 // The programs usage help.
@@ -17,11 +17,33 @@ Usage:
 	dwi <file>
 `
 
+// FileIndex provides methods for manipulating a list of files.
+type FileIndex interface {
+}
+
+// LocalFiles is a FileIndex for files in the local filesystem.
+type LocalFiles struct {
+	Root string
+}
+
+// NewLocalFiles creates and initializes a new LocalFiles with a given path root.
+func NewLocalFiles(root string) LocalFiles {
+	return LocalFiles{root}
+}
+
 // Main function.
-func Main(args []string, out io.Writer, eout io.Writer) {
+func Main(args []string, out io.Writer, eout io.Writer, fm FileIndex) {
 	if len(args) < 2 {
 		fmt.Fprintln(out, Usage)
 		return
 	}
-	fmt.Fprintln(out, "Your filename was:", args[1])
+
+	files := ParseArgs(args)
+	fmt.Fprintln(out, files)
+}
+
+// ParseArgs takes a list of args and returns the list of files.
+// The first arg must be the executable name.
+func ParseArgs(args []string) []string {
+	return args[1:]
 }
--- dwi_test.go
+++ dwi_test.go
@@ -8,7 +8,8 @@ import (
 func TestMainNoArgsShouldPrintUsage(t *testing.T) {
 	out := bytes.NewBufferString("")
 	err := bytes.NewBufferString("")
-	Main([]string{""}, out, err)
+
+	Main([]string{""}, out, err, NewLocalFiles("/"))
 
 	if out.String() != Usage+"\n" {
 		t.Error("Did not print usage.")
